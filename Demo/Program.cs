930100aaf0e46a18b64fdd662ad095d634ff6592
Miha Zupan <mihazupan.zupan1@gmail.com>
2022-05-06 16:28:20 +0200

    Add DnsRoundRobinConnector

---
+++ Demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using DnsRoundRobin;
+using System.Net;
+using System.Net.Sockets;
+
+var roundRobinConnector = new DnsRoundRobinConnector(
+    dnsRefreshInterval: TimeSpan.FromSeconds(10),
+    endpointConnectTimeout: TimeSpan.FromSeconds(5));
+
+const string TestHost = "httpbin.org";
+
+Console.WriteLine($"{TestHost} resolved to:");
+foreach (IPAddress address in await Dns.GetHostAddressesAsync(TestHost))
+{
+    Console.WriteLine(address);
+}
+Console.WriteLine();
+
+for (int i = 0; i < 100; i++)
+{
+    using var handler = new SocketsHttpHandler
+    {
+        ConnectCallback = async (context, cancellation) =>
+        {
+            Socket socket = await roundRobinConnector.ConnectAsync(context.DnsEndPoint, cancellation);
+
+            Console.WriteLine($"Connected to {socket.RemoteEndPoint}");
+
+            return new NetworkStream(socket, ownsSocket: true);
+        }
+    };
+
+    using var client = new HttpClient(handler);
+
+    await client.GetStringAsync($"https://{TestHost}");
+
+    await Task.Delay(1000);
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # DnsRoundRobin
-A helper for connecting Sockets to endpoints in a round-robin fashion
+
+A helper for connecting Sockets to endpoints in a round-robin fashion.
+
+```c#
+var handler = new SocketsHttpHandler
+{
+    ConnectCallback = async (context, cancellation) =>
+    {
+        Socket socket = await DnsRoundRobinConnector.Shared.ConnectAsync(context.DnsEndPoint, cancellation);
+
+        return new NetworkStream(socket, ownsSocket: true);
+    }
+};
+```
+
+You can configure things like the dns refresh interval or per-endpoint connection timeouts.
+
+```c#
+private static readonly DnsRoundRobinConnector s_roundRobinConnector = new(
+    dnsRefreshInterval: TimeSpan.FromSeconds(10),
+    endpointConnectTimeout: TimeSpan.FromSeconds(5));
+```
